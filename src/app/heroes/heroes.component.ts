e5c9a814bcadd7cef1d3ec441cf8426207722d63
beltranaugusto <beltranliscano@gmail.com>
2023-04-08 01:52:54 -0400

    Finished tour of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -21,10 +21,19 @@ export class HeroesComponent {
         .subscribe(heroes => this.heroes = heroes);
   }
   
-  // onSelect(hero: Hero): void {
-  //   this.selectedHero = hero;
-  //   this.messageService.add(`HeroesComponent: Selected Hero id=${hero.id}`);
-  // }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
 
   ngOnInit(): void {
     this.getHeroes()
